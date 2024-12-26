9e237e6ea6f6a79647455b4711cafafc59e4040b
Salma-007 <salmaelallali11@gmail.com>
2024-12-26 20:58:21 +0100

    adding the delete method

--- player_dynamic.php
+++ player_dynamic.php
@@ -1,13 +1,18 @@
 <?php 
 include_once('./db_conn.php');
 $conn = DatabaseConnection::connect();
+
 class PlayerDynamic{
     private $conn;
 
     public function __construct($conn) {
-        $this->conn = $conn;
+        $this->setConn($conn);
     } 
+    public function setConn($conn){
+        $this->conn = $conn;
+    }
 
+    // methode d'insertion joueur
     function insertRecord($conn, $table, $data) {
         // Use prepared statements to prevent SQL injection
         $columns = implode(',', array_keys($data));
@@ -20,19 +25,39 @@ class PlayerDynamic{
         if (!$stmt) {
             die("Error in prepared statement: " . print_r($conn->errorInfo(), true));
         }
-
         // Execute the prepared statement
         $result = $stmt->execute(array_values($data));
-
         return $result;
     }
+
+
+    // methode de suppression d'un joueur
+    function deleteRecord($conn, $table, $id) {
+        // Use prepared statements to prevent SQL injection
+        $sql = "DELETE FROM $table WHERE id = :id";
+        
+        $stmt = $conn->prepare($sql);
+        
+        if (!$stmt) {
+            die("Error in prepared statement: " . print_r($conn->errorInfo(), true));
+        }
+        // Execute the prepared statement with the parameters
+        $result = $stmt->execute([':id' => $id]);
+        
+        return $result;
+    }
+
+    // methode de update
+    
 
 }
 
 
 // Insert example
 $player1 = new PlayerDynamic($conn);
-$insertData = array('name' => 'taha', 'club' => 'Man City','nationality' => 'England','rating' => 55);
-$player1->insertRecord($conn, 'players', $insertData);
+// $insertData = array('name' => 'taha', 'club' => 'Man City','nationality' => 'England','rating' => 55);
+// $player1->insertRecord($conn, 'players', $insertData);
+// $player1->deleteRecord($conn,'players', 2);
+
 
 ?>
