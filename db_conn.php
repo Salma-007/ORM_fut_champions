8bd9cc95bad65e97151667426598bc972ec19a92
Salma-007 <salmaelallali11@gmail.com>
2024-12-26 16:16:08 +0100

    connection en PDO

--- db_conn.php
+++ db_conn.php
@@ -1,27 +1,26 @@
 <?php
 
-class Connect_db{
-    private $host;
-    private $password;
-    private $username;
-    private $dbname;
-    private $pdo;
+// using singleton pattern
+class DatabaseConnection{
+    protected static $connection = false;
+    private function __construct()
+    {
+        echo 'instance created';
+    }
 
-    public function __construct($host = 'localhost', $dbname = 'orm_fut_champions', $username = 'root', $password = '', $charset = "utf8mb4") {
-        try {
-            $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
-            $this->pdo = new PDO($dsn, $username, $password);
-            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $e) {
-            die("Erreur de connexion : " . $e->getMessage());
+    public static function connect(){
+        if(!self::$connection){
+            $conn = new PDO ("mysql:host=localhost;dbname=orm_fut_champions","root","");
+            self::$connection = $conn;
         }
+        return self::$connection;
     }
 
-    public function getConnection() {
-        return $this->pdo;
-    }
+
 }
 
-$dbb = new Connect_db();
-$dbb->getConnection(); 
+var_dump(DatabaseConnection::connect());
+var_dump(DatabaseConnection::connect());
+
+
 ?>
